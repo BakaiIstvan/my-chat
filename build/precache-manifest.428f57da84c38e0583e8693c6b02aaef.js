4abcec21b6dc1d8a58f8a6f334c4cb05454947e2
TheBattleDonut <37209918+TheBattleDonut@users.noreply.github.com>
2020-05-04 12:47:36 +0200

    Added build folder

---
+++ build/precache-manifest.428f57da84c38e0583e8693c6b02aaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddfccd750ca8061c9f839f095407fc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c23a2f3b40a46857de4d",
+    "url": "./static/css/main.96585dd0.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23a2f3b40a46857de4d",
+    "url": "./static/js/main.f2fce620.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
